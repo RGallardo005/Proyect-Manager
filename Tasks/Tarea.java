7a727e081e3931779de7a1cff2884c68d06ebf1f
Ricardo Gallardo <gallardoespinozar@gmail.com>
2024-08-01 16:35:47 -0600

    se crea comentariosr
    se crea los atributos y se le hacen las funciones principales se crea un arraylist y en el momento de agrgregar tiene un pequeno error2

--- Tasks/Tarea.java
+++ Tasks/Tarea.java
@@ -4,10 +4,59 @@
  */
 package Tasks;
 
+import Comentarios.Comentarios;
+import java.util.ArrayList;
+import users.user;
+
 /**
  *
  * @author galla
  */
 public class Tarea {
+    private int id;
+    private String description;
+    private Boolean completed;
+    private user user;
+    private ArrayList<Comentarios> Listcomentario;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public user getUser() {
+        return user;
+    }
+
+    public ArrayList<Comentarios> getListcomentario() {
+        return Listcomentario;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    public void addcomentario(Comentarios Comentarios){ 
+        Listcomentario.add(new Comentarios);
+    }
+    public Tarea(int id, String description, Boolean completed, user user, ArrayList<Comentarios> Listcomentario) {
+        this.id = id;
+        this.description = description;
+        this.completed = completed;
+        this.user = user;
+        this.Listcomentario = new ArrayList();
+    }
+
+    public Tarea() {
+        this(0,"",null,null,new ArrayList())
+    }
+
     
 }
